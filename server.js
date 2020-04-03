e5cd7f40c603e3518e3a4a8d13a1ee79007254f2
Ryosuke Fuse <fuseryosuke.0122@gmail.com>
2020-04-03 18:56:00 +0900

    add log configure

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express');
 
 const app = express();
+
 var log4js = require("log4js");
+
+log4js.configure({
+  "appenders": [{
+      "type": "file",
+      "filename": "log/system.log",
+      "maxLogSize": 20480,
+      "backups": 3,
+      "category": "system"
+    },
+    {
+      "type": "console"
+    }
+  ],
+  "levels": {
+    "system": "ALL"
+  }
+});
+
 var logger = log4js.getLogger("system");
 
 app.use(log4js.connectLogger(logger, {
