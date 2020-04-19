1ae6d3b4a71bbc3a78ca0f554db408fdefcdc505
Ryosuke Fuse <fuseryosuke.0122@gmail.com>
2020-04-19 22:05:48 +0900

    mod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_HOST: "http://localhost:8080",
+  API_HOST: "https://frysk-pokemon-quiz-develop.herokuapp.com/",
   MONGDB_URI:
     "mongodb://heroku_w6g57tmq:k73q0144qan0o52v2io1f20osh@ds123728.mlab.com:23728/heroku_w6g57tmq",
   production: true
