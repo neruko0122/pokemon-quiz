77c74a145fed8bfc66a525457c0d5fbf22463756
Ryosuke Fuse <fuseryosuke.0122@gmail.com>
2020-04-24 19:33:12 +0900

    add polyfills

--- src/app/shared/animations/move.ts
+++ src/app/shared/animations/move.ts
@@ -7,7 +7,43 @@ export const moveUpAnimation = animation([
   animate(
     '1s',
     style({
-      transform: 'translateY(-100px)'
+      transform: 'translateY(-50px)'
+    })
+  )
+])
+
+export const moveDownAnimation = animation([
+  style({
+    transform: '*'
+  }),
+  animate(
+    '1s',
+    style({
+      transform: 'translateY(50px)'
+    })
+  )
+])
+
+export const moveRightAnimation = animation([
+  style({
+    transform: '*'
+  }),
+  animate(
+    '1s',
+    style({
+      transform: 'translateX(50px)'
+    })
+  )
+])
+
+export const moveLeftAnimation = animation([
+  style({
+    transform: '*'
+  }),
+  animate(
+    '1s',
+    style({
+      transform: 'translateX(-50px)'
     })
   )
 ])
