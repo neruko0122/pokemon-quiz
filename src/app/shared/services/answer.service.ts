4936254b6912f502a74ae115fd464bbeb0a30a0d
Ryosuke Fuse <ryosuke.fuse@fullmarks.co.jp>
2024-06-07 16:40:59 +0900

    起動時エラー調整

--- src/app/shared/services/answer.service.ts
+++ src/app/shared/services/answer.service.ts
@@ -1,41 +1,41 @@
-import { Injectable } from '@angular/core'
-import { Http, Response } from '@angular/http'
-import { Observable } from 'rxjs'
-import { environment } from 'src/environments/environment'
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
-import { QUESTIONS } from '../constants'
+import { QUESTIONS } from "../constants";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AnswerService {
-  answerList: any[]
-  constructor(private http: Http) {}
+  answerList: any[];
+  constructor(private http: HttpClient) {}
 
   clearList() {
-    this.answerList = []
+    this.answerList = [];
   }
 
   addAnswer(target, quizType, choice: string, correctAnswer, imageUrl) {
     this.answerList.push({
-      no: target['number'],
+      no: target["number"],
       question: QUESTIONS[quizType].value,
       answer: choice,
       correctAnswer: correctAnswer,
       success: correctAnswer === choice,
       image: imageUrl
-    })
+    });
   }
 
   getAnswer() {
-    return this.answerList
+    return this.answerList;
   }
 
-  findAnswer(): Observable<Response> {
-    return this.http.get(environment.API_HOST + '/api/answers')
+  findAnswer(): Observable<any> {
+    return this.http.get(environment.API_HOST + "/api/answers");
   }
 
-  createAnswer(answer): Observable<Response> {
-    return this.http.post(environment.API_HOST + '/api/answers', answer)
+  createAnswer(answer): Observable<any> {
+    return this.http.post(environment.API_HOST + "/api/answers", answer);
   }
 }
--- src/app/shared/services/ranking.service.ts
+++ src/app/shared/services/ranking.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@angular/core'
-import { Http } from '@angular/http'
-import { environment } from 'src/environments/environment'
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class RankingService {
-  constructor(private http: Http) {}
+  constructor(private http: HttpClient) {}
 
   findRanking() {
-    return this.http.get(environment.API_HOST + '/api/ranking')
+    return this.http.get(environment.API_HOST + "/api/ranking");
   }
 
   registerRanking(result) {
-    return this.http.post(environment.API_HOST + '/api/ranking', result)
+    return this.http.post(environment.API_HOST + "/api/ranking", result);
   }
 }
